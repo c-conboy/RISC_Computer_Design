1d4b56abc507a00681a35c1bb08a56108ac4aa10
18mgp5 <18mgp5@queensu.ca>
2023-03-13 15:09:01 -0400

    added config files

--- simulation/modelsim/rtl_work/ld_datapath_tb/_primary.vhd
+++ simulation/modelsim/rtl_work/ld_datapath_tb/_primary.vhd
@@ -1,6 +1,6 @@
 library verilog;
 use verilog.vl_types.all;
-entity mflo_datapath_tb is
+entity ld_datapath_tb is
     generic(
         Default         : vl_logic_vector(0 to 3) := (Hi0, Hi0, Hi0, Hi0);
         Reg_load1a      : vl_logic_vector(0 to 3) := (Hi0, Hi0, Hi0, Hi1);
@@ -13,7 +13,10 @@ entity mflo_datapath_tb is
         T1              : vl_logic_vector(0 to 3) := (Hi1, Hi0, Hi0, Hi0);
         T2              : vl_logic_vector(0 to 3) := (Hi1, Hi0, Hi0, Hi1);
         T3              : vl_logic_vector(0 to 3) := (Hi1, Hi0, Hi1, Hi0);
-        T4              : vl_logic_vector(0 to 3) := (Hi1, Hi0, Hi1, Hi1)
+        T4              : vl_logic_vector(0 to 3) := (Hi1, Hi0, Hi1, Hi1);
+        T5              : vl_logic_vector(0 to 3) := (Hi1, Hi1, Hi0, Hi0);
+        T6              : vl_logic_vector(0 to 3) := (Hi1, Hi1, Hi0, Hi1);
+        T7              : vl_logic_vector(0 to 3) := (Hi1, Hi1, Hi1, Hi0)
     );
     attribute mti_svvh_generic_type : integer;
     attribute mti_svvh_generic_type of Default : constant is 1;
@@ -28,4 +31,7 @@ entity mflo_datapath_tb is
     attribute mti_svvh_generic_type of T2 : constant is 1;
     attribute mti_svvh_generic_type of T3 : constant is 1;
     attribute mti_svvh_generic_type of T4 : constant is 1;
-end mflo_datapath_tb;
+    attribute mti_svvh_generic_type of T5 : constant is 1;
+    attribute mti_svvh_generic_type of T6 : constant is 1;
+    attribute mti_svvh_generic_type of T7 : constant is 1;
+end ld_datapath_tb;
